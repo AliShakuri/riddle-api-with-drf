bdd5ed80be9a68390d5cd097fc128b8878f75b43
alishakuri <ali95shakur@gmail.com>
2021-10-17 15:19:51 +0330

    set default permissions and edit serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
 from .models import Riddle, Comment
 
+
 class CommentSerializers(serializers.ModelSerializer):
+    def get_username(self, obj):
+        return {
+            "username": obj.user.username
+        }
+
+    user = serializers.SerializerMethodField("get_username")
+
     class Meta:
         model = Comment
         fields = "__all__"
+
 
 class RiddleSerializers(serializers.ModelSerializer):
     def get_comments(self, obj):
@@ -16,4 +25,4 @@ class RiddleSerializers(serializers.ModelSerializer):
 
     class Meta:
         model = Riddle
-        fields = ['title', 'text', 'publish', 'created', 'updated', 'comments']
+        fields = ['id', 'title', 'text', 'publish', 'created', 'updated', 'comments']
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     'rest_framework',
     'rest_framework.authtoken',
+    "corsheaders",
     'drf_yasg',
     'djoser',
     "api.apps.ApiConfig"
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -136,6 +138,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'api.permissions.IsStaffOrReadOnly',
     ],
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
